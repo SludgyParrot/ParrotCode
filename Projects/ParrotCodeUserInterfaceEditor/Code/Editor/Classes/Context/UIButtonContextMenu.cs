6fc99e6f14e64dc230f1798f75ecebb33f1c049f
Sludgy Parrot <sludgyparrot@gmail.com>
2026-06-03 22:09:33 +0200

    Added UI editor project.

---
+++ Projects/ParrotCodeUserInterfaceEditor/Code/Editor/Classes/Context/UIButtonContextMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using ParrotCode.UI;
+
+namespace ParrotCode.UIEditor
+{
+    public class UIButtonContextMenu: Editor
+    {
+        private const string ButtonMenuName = EditorSharedStrings.ContextRootMenuName + "UI Button";
+        private const int UIMenuPriority = 0;
+        private const bool Validate = false;
+
+
+        [MenuItem(ButtonMenuName, validate = Validate, priority = UIMenuPriority)]
+        private static void CreateUIButton(MenuCommand menu)
+        {
+            GameObject parent = menu.context as GameObject;
+
+            if(parent == null)
+            {
+                Debug.Log("~There's no selected game object.");
+            }
+            else
+            {
+                Debug.Log($"~Selected parent game object called: {parent.name}.");
+            }
+        }
+
+    }
+
+}
---
+++ Projects/ParrotCodeUserInterfaceEditor/Code/Editor/Classes/Shared/CustomEditorSharedStrings.cs
@@ -0,0 +1,7 @@
+﻿namespace ParrotCode.UIEditor
+{
+    public static class EditorSharedStrings
+    {
+        public const string ContextRootMenuName = "GameObject/Sludgy Parrot/UI/";
+    }
+}
